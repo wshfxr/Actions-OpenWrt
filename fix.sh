fe3c7536f86b209facee4e6be8b08707233bf09e
pikachullk <pikachullk@gmail.com>
2019-12-09 00:54:24 +0800

    Create fix.sh

---
+++ fix.sh
@@ -0,0 +1,13 @@
+rm -rf feeds/lienol/lienol/v2ray
+rm -rf feeds/lienol/lienol/openssl1.1
+rm -rf feeds/lienol/lienol/trojan
+rm -rf feeds/lienol/lienol/shadowsocks-libev-new
+rm -rf feeds/lienol/lienol/shadowsocksr-libev
+rm -rf feeds/lienol/lienol/pdnsd-alt
+rm -rf package/lean/kcptun
+rm -rf package/lean/verysync
+rm -rf package/lean/luci-app-kodexplorer
+rm -rf package/lean/luci-app-pppoe-relay
+rm -rf package/lean/luci-app-pptp-server
+rm -rf package/lean/luci-app-v2ray-server
+rm -rf package/lean/luci-app-verysync
