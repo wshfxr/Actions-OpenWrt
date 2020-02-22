53d68e34f9b50af67eb56e33b2e5d9bfde5c1edb
pikachullk <pikachullk@gmail.com>
2020-02-22 08:21:23 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,5 +9,5 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/project-openwrt/luci-app-unblockneteasemusic package/lean/luci-app-unblockneteasemusic
 git clone https://github.com/tty228/luci-app-serverchan package/lean/luci-app-serverchan
-git clone https://github.com/jerrykuku/luci-theme-argon package/lean/luci-theme-argon-mc
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon-mc
 
